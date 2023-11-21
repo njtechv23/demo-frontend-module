c55712f201b82fe22f85917c55578c319e7412ee
Nibjyoti Nath <nibjyoti.n@techvibrant.com>
2023-11-21 13:41:20 +0530

    demo contents for demo repo

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const demoModule = () => {
+    return (
+        <View>
+            <Text>A common module, just a demo</Text>
+        </View>
+    )
+}
+
+export default demoModule;
