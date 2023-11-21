66fc54c7c9fd9eef6ab45ead366a4cf4d283c726
Nibjyoti Nath <nibjyoti.n@techvibrant.com>
2023-11-21 16:45:53 +0530

    included gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+const DemoModule = () => {
+    return (
+        <View>
+            <Text>A common module, just a demo</Text>
+        </View>
+    )
+}
+
+export default DemoModule;
